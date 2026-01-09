65c6732c898dd79d6dd16f01b303d27eedeb35df
zNyx <152056642+OsitoXc@users.noreply.github.com>
2026-01-09 04:55:15 -0600

    Actualizar navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -2,9 +2,3 @@ function toggleMenu(){
   const menu = document.getElementById("menu");
   menu.classList.toggle("active");
 }
-
-document.querySelectorAll('#menu a').forEach(link => {
-  link.addEventListener('click', () => {
-    document.getElementById('menu').classList.remove('active');
-  });
-});
