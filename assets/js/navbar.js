7e95abf9297715b00994cf7762d22b39ba131705
zNyx <152056642+OsitoXc@users.noreply.github.com>
2026-03-03 12:15:54 -0600

    Actualizar navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -3,13 +3,12 @@ function toggleMenu(){
   menu.classList.toggle("active");
 }
 
-<script>
-  window.addEventListener("load", function(){
-    const loader = document.getElementById("loader");
+window.addEventListener("load", function(){
+  const loader = document.getElementById("loader");
 
-    // Evita parpadeo si carga demasiado rápido
+  if(loader){
     setTimeout(() => {
       loader.classList.add("hidden");
     }, 700);
-  });
-</script>
+  }
+});
