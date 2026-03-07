bcbccd3a7bad370ad62872ab32db3bb9e363eda0
zNyx <152056642+OsitoXc@users.noreply.github.com>
2026-03-07 13:41:45 -0600

    Actualizar auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,51 +1,34 @@
 const supabaseClient = supabase.createClient(
-"https://imoubtvtadgzaxkbuils.supabase.co",
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imltb3VidHZ0YWRnemF4a2J1aWxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5MDE2MTEsImV4cCI6MjA4ODQ3NzYxMX0.1vcQI1LwcuT9ssxdfnnAuHJJiMNig2hMZCs-efJWP0E"
+  "https://imoubtvtadgzaxkbuils.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imltb3VidHZ0YWRnemF4a2J1aWxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5MDE2MTEsImV4cCI6MjA4ODQ3NzYxMX0.1vcQI1LwcuT9ssxdfnnAuHJJiMNig2hMZCs-efJWP0E"
 );
 
-async function loadUser(){
+async function loadUser() {
+  const box = document.getElementById("userBox");
 
-const box = document.getElementById("userBox");
+  const { data } = await supabaseClient.auth.getUser();
+  const user = data.user;
 
-const { data } = await supabaseClient.auth.getUser();
+  if (user) {
+    const avatar = user.user_metadata.avatar_url;
+    const name = user.user_metadata.full_name || user.user_metadata.name;
 
-const user = data.user;
-
-if(user){
-
-const avatar = user.user_metadata.avatar_url;
-const name = user.user_metadata.full_name;
-
-box.innerHTML = `
-<img src="${avatar}" style="width:32px;height:32px;border-radius:50%;">
-<span>${name}</span>
-
-}else{
-
-box.innerHTML = `
-<button onclick="login()" class="login-btn">Discord</button>
-`;
-
+    box.innerHTML = `
+      <img src="${avatar}" class="user-avatar">
+      <span class="user-name">${name}</span>
+    `;
+  } else {
+    box.innerHTML = `<button onclick="login()" class="login-btn">Login</button>`;
+  }
 }
 
-}
-
-async function login(){
-
-await supabaseClient.auth.signInWithOAuth({
-provider:"discord",
-options:{
-redirectTo: window.location.origin
-}
-});
-
-}
-
-async function logout(){
-
-await supabaseClient.auth.signOut();
-location.reload();
-
+async function login() {
+  await supabaseClient.auth.signInWithOAuth({
+    provider: "discord",
+    options: {
+      redirectTo: window.location.origin,
+    },
+  });
 }
 
 window.addEventListener("load", loadUser);
